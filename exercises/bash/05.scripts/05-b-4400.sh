e6147a6788ef57f5934ac2a37fbe7ae44a6e715c
Radina Dineva <32228357+rdineva@users.noreply.github.com>
2022-04-15 10:14:49 +0300

    Update 05-b-4400.sh

--- exercises/bash/05.scripts/05-b-4400.sh
+++ exercises/bash/05.scripts/05-b-4400.sh
@@ -8,6 +8,25 @@
 # Ако втората директория не е подадена по име, нека да получи такова от днешната дата във формат, който ви е удобен.
 # При желание новосъздадената директория да се архивира.
 
+[ $# -ne 1 ] && [ $# -ne 2 ] && echo "ERROR: 1 or 2 params needed" && exit 1
+
+[ ! -d $1 ] && echo "ERROR: $1 is not a directory" && exit 2
+
+[ $# -eq 2 ] && [ ! -d $2 ] && echo "ERROR: $2 is not a directory" && exit 3
+
+dest=$2
+src=$1
+
+if [ ! $# -eq 2 ]; then
+	dest=$(date +"%d-%m-%y %H:%M:%S" | tr ' ' '_')
+   	mkdir $dest
+fi
+
+cp $(find $src -maxdepth 1 -type f -cmin -45) $dest
+tar -cf "$dest.tar" $dest
+
+### OLDER SOLUTION
+
 if [ ! $# -eq 1 ] && [ ! $# -eq 2 ]; then
     echo "ERROR: Incorrect number of params."
     exit 1
